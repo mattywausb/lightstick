9a09f24c780537a0e0309412677b7ceade987aad
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2020-07-27 22:34:41 +0200

    String to sequence interpreter added

--- LightShow_preparation/LightShow_preparation.ino
+++ LightShow_preparation/LightShow_preparation.ino
@@ -10,6 +10,7 @@
 #ifdef TRACE_ON
 #define TRACE_SEQUENCE_PROGRESS
 #define TRACE_BUTTON_INPUT
+#define TRACE_STRING_PARSING
 #endif
 
 typedef struct {
@@ -19,14 +20,14 @@ typedef struct {
 } t_program_slot;
 
 t_program_slot g_program_slot[MAX_NUMBER_OF_PROGRAM_SLOTS]={ /* IDOL 126 BPM*/
-                               {'A',3,STEP_ON_16TH}
-                              ,{'B',5,STEP_ON_8TH}
-                              ,{'C',1,STEP_ON_BEAT}
-                              ,{'D',5,STEP_ON_16TH}
-                              ,{PROGRAM_TAG_OFF,STEP_ON_8TH}
-                              ,{PROGRAM_TAG_OFF,STEP_ON_8TH}
-                              ,{PROGRAM_TAG_OFF,STEP_ON_8TH}
-                              ,{PROGRAM_TAG_OFF,STEP_ON_8TH}
+                               {'R',3,STEP_ON_16TH}
+                              ,{'A',5,STEP_ON_8TH}
+                              ,{'B',1,STEP_ON_BEAT}
+                              ,{'S',5,STEP_ON_16TH}
+                              ,{'I',0,STEP_ON_BEAT}
+                              ,{PROGRAM_TAG_OFF,0,STEP_ON_8TH}
+                              ,{PROGRAM_TAG_OFF,0,STEP_ON_8TH}
+                              ,{PROGRAM_TAG_OFF,0,STEP_ON_8TH}
                               };
                   
 
@@ -41,9 +42,10 @@ int g_program_sequence_length=5;
 
 
 t_program_sequence g_program_sequence [MAX_NUMBER_OF_PROGRAM_SEQUENCE_STEPS] = {
-                      {0,16},{1,64},{2,32},{0,32},{3,32}
-                      };
-                     
+                      {5,16},{1,64},{2,32},{0,32},{3,32}
+                      }; // Extended Version to S126 I88 >A8888 8888 B8888 R8888 S8888 A8888 8888 B8888 R8888 S8888 B8888 R8888 S8888 A8888 
+
+// Other test: S80 A22 B4 >C8 D4                     
                         
 int g_sequence_entry_count=8;
 int g_sequence_index=0;
@@ -139,6 +141,74 @@ void trace_sequence()
 }
 #endif
 
+/* Function to load the program sequence by parsing a string code 
+* Syntax: <bpmdigit>{2..3}<letter>{1}<beatdigit>{1..n}
+* <bpmdigit>: declars the BPM Speed
+* <letter>: declares the slot_tag that is addressed 
+* <beatdigit>: declares number of beats to hold the slot, consecutive digits will be added (e.g. 248=14)
+* Special characters: > Mark Sequence entry to loop back (last entry counts)
+*/
+
+void parse_sequence(String sequence_string)  
+{
+  int newBPM=sequence_string.toInt();
+  int char_index=0;
+  int sequence_index=0;
+  int beat_sum=0;
+  int slot_index=-1;
+  g_program_sequence_loop_entry_index=0;
+
+  #ifdef TRACE_STRING_PARSING
+         Serial.print(F(">TRACE_STRING_PARSING string to parse:"));Serial.println(sequence_string);
+  #endif
+  
+  for(char_index=0;char_index<sequence_string.length();char_index++) {
+    if(sequence_string.charAt(char_index)>='A') { // new slot letter in string
+      if(beat_sum>0 && slot_index>=0) { // previous phrase is complete and valid
+        g_program_sequence[sequence_index].slot_index=slot_index;
+        g_program_sequence[sequence_index].beats_to_run=beat_sum;
+        #ifdef TRACE_STRING_PARSING
+         Serial.print(F(">TRACE_STRING_PARSING sequence_index:"));Serial.print(sequence_index);
+         Serial.print(F(" slot_index:"));Serial.print(slot_index);
+         Serial.print(F(" beat_sum:"));Serial.println(beat_sum);
+        #endif
+        sequence_index++;
+      }
+      beat_sum=0;
+      for(slot_index=MAX_NUMBER_OF_PROGRAM_SLOTS-1;slot_index>=0;slot_index--) { // find proper slot
+        if(sequence_string.charAt(char_index)==g_program_slot[slot_index].slot_tag) break;
+      }
+      #ifdef TRACE_STRING_PARSING
+         Serial.print(F(">TRACE_STRING_PARSING slot_index:"));Serial.println(slot_index);
+      #endif
+    } else 
+    if(sequence_string.charAt(char_index)>='1' && sequence_string.charAt(char_index)<='9') { // beat digit
+      beat_sum+=sequence_string.substring(char_index,char_index+1).toInt();
+    } else
+    if(sequence_string.charAt(char_index)>='>') {  // Loop marker
+      g_program_sequence_loop_entry_index=sequence_index+1;
+      #ifdef TRACE_STRING_PARSING
+         Serial.print(F(">TRACE_STRING_PARSING loop entry at:"));Serial.println(g_program_sequence_loop_entry_index);
+      #endif
+    }
+  } // end loop over characters of the string
+  if(beat_sum>0 || slot_index>=0) { // previous phrase is complete and valid
+        g_program_sequence[sequence_index].slot_index=slot_index;
+        g_program_sequence[sequence_index].beats_to_run=beat_sum;
+         #ifdef TRACE_STRING_PARSING
+         Serial.print(F(">TRACE_STRING_PARSING sequence_index:"));Serial.print(sequence_index);
+         Serial.print(F(" slot_index:"));Serial.print(slot_index);
+         Serial.print(F(" beat_sum:"));Serial.println(beat_sum);
+        #endif
+        sequence_index++;
+      }
+  g_program_sequence_length=sequence_index;
+  if(g_program_sequence_loop_entry_index>=g_program_sequence_length)g_program_sequence_loop_entry_index=0;  // in case loop entry tag is placed after last valid block
+  
+  if(newBPM>0) output_set_bpm(newBPM);
+  sequence_start();
+}
+
 void sequence_start()
 {
   g_sequence_index=0;
@@ -176,13 +246,17 @@ void loop() {
   input_pollSerial();
   if(input_newSerialCommandAvailable()) {
     String command=input_getSerialCommand();
+    if(command.startsWith("S")) { //Change sequence by string
+      parse_sequence(command.substring(1));
+      mode_of_operation=MODE_SEQUENCE;
+    }
+    if(command.startsWith("s")) {  // restart sequence mode
+      mode_of_operation=MODE_SEQUENCE;
+      sequence_start();
+    }
     if(command.startsWith("b")) {  // b123 <- Change bpm: 
       int bpm=command.substring(1).toInt();
       if(bpm>0) output_set_bpm(bpm);
-    }
-    if(command.startsWith("s")) {  // trigger sequence mode
-      mode_of_operation=MODE_SEQUENCE;
-      sequence_start();
     }
     if(command.startsWith("w")) {  // w16 <- Change waittime
       int wait_value=command.substring(1).toInt();
