b8ac150cfc36ddc14a00df2494db9cb36f17a079
Matthias Wegner <19684192+mattywausb@users.noreply.github.com>
2020-07-29 21:39:42 +0200

    Refactored variable names in output module

--- LightShow_preparation/output.ino
+++ LightShow_preparation/output.ino
@@ -31,14 +31,15 @@ typedef struct {
     float s;       // a fraction between 0 and 1
 } t_color_hs;
 
-#define COLOR_PALETTE_MAX_ENTRIES  4 
-t_color_hs g_color_palette[COLOR_PALETTE_MAX_ENTRIES];
-uint8_t g_color_palette_lenght=0;
-uint8_t g_color_palette_index=0;
+#define COLOR_PALETTE_MAX_ENTRIES  6 
+t_color_hs patconf_color_palette[COLOR_PALETTE_MAX_ENTRIES];
+uint8_t patconf_color_palette_lenght=0;
+uint8_t patvar_color_palette_index=0;
 
 enum STEPPER_TYPE {
   ST_COLOR_WIPE,
   ST_DOUBLE_ORBIT,
+  ST_DOUBLE_COLOR_ORBIT,
   ST_RAINBOW
 };
 
@@ -46,27 +47,25 @@ STEPPER_TYPE g_current_stepper_type=ST_COLOR_WIPE;
 
 
 
-int g_output_waittime[6];
-int g_beats_per_minute = 120;
-int g_pattern_step_wait_index=STEP_ON_BEAT;
+int output_waittime[6];
+int output_beats_per_minute = 120;
+float output_master_light_value = 0.5;
 long output_beat_sync_time=0L;
 int output_preset_beat_start_beat=0;
 int output_preset_beat_count=0;
 
 
-boolean g_pattern_needs_init = false;
+long output_pattern_previous_step_time = 0L;
 
-long g_pattern_previous_step_time = 0L;
-int g_pattern_step_index = 0;
-uint8_t g_steps_per_color=0;
-uint8_t g_step_in_color_index=0;
-int g_max_step_count=1000;
+int patconf_speed_id=STEP_ON_BEAT;
+uint8_t patconf_steps_until_color_change=0;
+uint8_t patconf_step_color_increment=1;
+int patconf_max_step_count=1000;
+float patconf_pattern_lamp_value = 0.5;
+float patconf_rainbow_step_angle_increment=1.0;
 
-float g_pattern_value = 0.5;
-float g_master_light_value = 0.5;
-float g_rainbow_step_angle_increment=1.0;
-
-
+int patvar_current_step_index = 0;
+uint8_t patvar_step_in_color_index=0;
 
 /* 
  *  API Functions
@@ -87,35 +86,35 @@ void output_set_bpm(int beats_per_minute) {
   #ifdef TRACE_OUTPUT_API_CALL
       Serial.print(F(">TRACE_OUTPUT_API_CALL output_set_bpm:"));Serial.println(beats_per_minute);
   #endif 
-  g_beats_per_minute = beats_per_minute;
-  output_set_waittimes(60000/g_beats_per_minute);
+  output_beats_per_minute = beats_per_minute;
+  output_set_waittimes(60000/output_beats_per_minute);
   output_sync_beat();
 };
 
-int output_get_bpm() { return g_beats_per_minute;}
+int output_get_bpm() { return output_beats_per_minute;}
 
 void output_set_waittimes(int time_for_beat) {
-  g_output_waittime[STEP_ON_2BEATS] =time_for_beat*2;
-  g_output_waittime[STEP_ON_BEAT] = time_for_beat;
-  g_output_waittime[STEP_ON_8TH] = g_output_waittime[STEP_ON_BEAT] /2;
-  g_output_waittime[STEP_ON_16TH] = g_output_waittime[STEP_ON_8TH] /2;
-  g_output_waittime[STEP_ON_32RD] = g_output_waittime[STEP_ON_16TH] /2;
-  g_output_waittime[STEP_ON_64TH] = g_output_waittime[STEP_ON_32RD] /2;
+  output_waittime[STEP_ON_2BEATS] =time_for_beat*2;
+  output_waittime[STEP_ON_BEAT] = time_for_beat;
+  output_waittime[STEP_ON_8TH] = output_waittime[STEP_ON_BEAT] /2;
+  output_waittime[STEP_ON_16TH] = output_waittime[STEP_ON_8TH] /2;
+  output_waittime[STEP_ON_32RD] = output_waittime[STEP_ON_16TH] /2;
+  output_waittime[STEP_ON_64TH] = output_waittime[STEP_ON_32RD] /2;
   #ifdef TRACE_OUTPUT_TIMING
     Serial.print(F(">TRACE_OUTPUT_TIMING calculated BPM ")); Serial.println(60000/time_for_beat);
-    Serial.print(F(">TRACE_OUTPUT_TIMING STEP_ON_BEAT ")); Serial.println(g_output_waittime[STEP_ON_BEAT]);
-    //Serial.print(F(">TRACE_OUTPUT_TIMING STEP_ON_32RD ")); Serial.println(g_output_waittime[STEP_ON_32RD]);
-    //Serial.print(F(">TRACE_OUTPUT_TIMING STEP_ON_64TH ")); Serial.println(g_output_waittime[STEP_ON_64TH]);
+    Serial.print(F(">TRACE_OUTPUT_TIMING STEP_ON_BEAT ")); Serial.println(output_waittime[STEP_ON_BEAT]);
+    //Serial.print(F(">TRACE_OUTPUT_TIMING STEP_ON_32RD ")); Serial.println(output_waittime[STEP_ON_32RD]);
+    //Serial.print(F(">TRACE_OUTPUT_TIMING STEP_ON_64TH ")); Serial.println(output_waittime[STEP_ON_64TH]);
   #endif
 }
 
 int output_get_beat_number_since_sync() {
   long runtime_since_sync=millis()-output_beat_sync_time;
-  return runtime_since_sync/g_output_waittime[STEP_ON_BEAT];
+  return runtime_since_sync/output_waittime[STEP_ON_BEAT];
 }
 
 long output_get_current_beat_start_time()  {
-   return output_get_beat_number_since_sync()*g_output_waittime[STEP_ON_BEAT]; 
+   return output_get_beat_number_since_sync()*output_waittime[STEP_ON_BEAT]; 
 }
 
 int output_get_preset_beat_count() {
@@ -124,7 +123,7 @@ int output_get_preset_beat_count() {
 
 void output_sync_beat() {
   output_beat_sync_time=millis();
-  g_pattern_previous_step_time=output_get_current_beat_start_time();
+  output_pattern_previous_step_time=output_get_current_beat_start_time();
 }
 
 // Change Pattern speed (relative to bpm)
@@ -133,7 +132,7 @@ void output_set_pattern_speed(int wait_index)
   #ifdef TRACE_OUTPUT_API_CALL
       Serial.print(F(">TRACE_OUTPUT_API_CALL output_set_pattern_speed:"));Serial.println(wait_index);
   #endif 
-  if(wait_index>=STEP_ON_2BEATS && wait_index<=STEP_ON_64TH) g_pattern_step_wait_index=wait_index;
+  if(wait_index>=STEP_ON_2BEATS && wait_index<=STEP_ON_64TH) patconf_speed_id=wait_index;
 }
 
 // Select and start a pattern
@@ -145,53 +144,53 @@ void output_start_preset(int preset_id) {
   output_preset_beat_count=0;
   switch (preset_id) {
     case 0:
-         g_color_palette[0].h=HUE_YELLOW;g_color_palette[0].s=1.0;
-         g_color_palette_lenght=1;
+         patconf_color_palette[0].h=HUE_YELLOW;patconf_color_palette[0].s=1.0;
+         patconf_color_palette_lenght=1;
          start_colorWipe(0.05,true);
          break;
     case 1:
-         g_color_palette[0].h=HUE_RED;g_color_palette[0].s=1.0;
-         g_color_palette[1].h=HUE_ORANGE;g_color_palette[1].s=1.0;
-         g_color_palette[2].h=HUE_RED;g_color_palette[2].s=0.8;
-         g_color_palette_lenght=3;
+         patconf_color_palette[0].h=HUE_RED;patconf_color_palette[0].s=1.0;
+         patconf_color_palette[1].h=HUE_ORANGE;patconf_color_palette[1].s=1.0;
+         patconf_color_palette[2].h=HUE_RED;patconf_color_palette[2].s=0.8;
+         patconf_color_palette_lenght=3;
          start_colorWipe(0.5,true);
          break;
     case 2:
-         g_color_palette[0].h=HUE_GREEN;g_color_palette[0].s=1.0;
-         g_color_palette[1].h=HUE_CYAN;g_color_palette[1].s=1.0; 
-         g_color_palette[2].h=HUE_GREEN;g_color_palette[2].s=1.0;
-         g_color_palette[3].h=HUE_LEMON;g_color_palette[3].s=1.0;
-         g_color_palette_lenght=4;
+         patconf_color_palette[0].h=HUE_GREEN;patconf_color_palette[0].s=1.0;
+         patconf_color_palette[1].h=HUE_CYAN;patconf_color_palette[1].s=1.0; 
+         patconf_color_palette[2].h=HUE_GREEN;patconf_color_palette[2].s=1.0;
+         patconf_color_palette[3].h=HUE_LEMON;patconf_color_palette[3].s=1.0;
+         patconf_color_palette_lenght=4;
          start_colorWipe(0.5,false);
          break;
     case 3:
-         g_color_palette[0].h=HUE_BLUE;g_color_palette[0].s=1.0;
-         g_color_palette[1].h=HUE_CYAN;g_color_palette[1].s=0.1; // WHITE
-         g_color_palette[2].h=HUE_RED;g_color_palette[2].s=1.0;
-         g_color_palette[3].h=HUE_CYAN;g_color_palette[3].s=0.1; // WHITE
-         g_color_palette_lenght=4;
+         patconf_color_palette[0].h=HUE_BLUE;patconf_color_palette[0].s=1.0;
+         patconf_color_palette[1].h=HUE_CYAN;patconf_color_palette[1].s=0.1; // WHITE
+         patconf_color_palette[2].h=HUE_RED;patconf_color_palette[2].s=1.0;
+         patconf_color_palette[3].h=HUE_CYAN;patconf_color_palette[3].s=0.1; // WHITE
+         patconf_color_palette_lenght=4;
          start_colorWipe(0.5,false);
          break;
     case 4:
-         g_color_palette[0].h=HUE_BLUE;g_color_palette[0].s=1.0;
-         g_color_palette[1].h=HUE_CYAN;g_color_palette[1].s=1.0; // WHITE
-         g_color_palette_lenght=2;
+         patconf_color_palette[0].h=HUE_BLUE;patconf_color_palette[0].s=1.0;
+         patconf_color_palette[1].h=HUE_CYAN;patconf_color_palette[1].s=1.0; // WHITE
+         patconf_color_palette_lenght=2;
          start_doubleOrbit(0.5,8);
          break;
     case 5:
-         g_color_palette[0].h=HUE_YELLOW;g_color_palette[0].s=1.0;
-         g_color_palette[1].h=HUE_PURPLE;g_color_palette[1].s=1.0;
-         g_color_palette[2].h=HUE_RED;g_color_palette[2].s=1.0; 
-         g_color_palette[3].h=HUE_GREEN;g_color_palette[3].s=1.0; 
-         g_color_palette_lenght=4;
+         patconf_color_palette[0].h=HUE_YELLOW;patconf_color_palette[0].s=1.0;
+         patconf_color_palette[1].h=HUE_PURPLE;patconf_color_palette[1].s=1.0;
+         patconf_color_palette[2].h=HUE_RED;patconf_color_palette[2].s=1.0; 
+         patconf_color_palette[3].h=HUE_GREEN;patconf_color_palette[3].s=1.0; 
+         patconf_color_palette_lenght=4;
          start_doubleOrbit(0.5,32);
          break;
     case 6:
-         g_color_palette[0].h=HUE_LEMON;g_color_palette[0].s=1.0;
-         g_color_palette[1].h=HUE_CYAN;g_color_palette[1].s=1.0;
-         g_color_palette[2].h=HUE_PINK;g_color_palette[2].s=0.7;  // PINK
-         g_color_palette[3].h=HUE_ORANGE;g_color_palette[3].s=1.0; 
-         g_color_palette_lenght=4;
+         patconf_color_palette[0].h=HUE_LEMON;patconf_color_palette[0].s=1.0;
+         patconf_color_palette[1].h=HUE_CYAN;patconf_color_palette[1].s=1.0;
+         patconf_color_palette[2].h=HUE_PINK;patconf_color_palette[2].s=0.7;  // PINK
+         patconf_color_palette[3].h=HUE_ORANGE;patconf_color_palette[3].s=1.0; 
+         patconf_color_palette_lenght=4;
          start_doubleOrbit(0.5,32);
          break;
     case 7:
@@ -240,32 +239,32 @@ void start_colorWipe(float lamp_value, boolean over_black){
 
   g_current_stepper_type=ST_COLOR_WIPE;
 
-  g_pattern_previous_step_time = output_get_current_beat_start_time() ;
-  g_pattern_step_index = 0;
-  g_max_step_count=over_black ?  (LAMP_COUNT - 1) * 2 : (LAMP_COUNT - 1) ; 
+  output_pattern_previous_step_time = output_get_current_beat_start_time() ;
+  patvar_current_step_index = 0;
+  patconf_max_step_count=over_black ?  (LAMP_COUNT - 1) * 2 : (LAMP_COUNT - 1) ; 
 
-  g_pattern_value = lamp_value;
-  g_color_palette_index=0;
+  patconf_pattern_lamp_value = lamp_value;
+  patvar_color_palette_index=0;
 
   // init all lamps
   lamp[0].set_value(0.0); // Switch of center lamp
   // Preset color, but set value to 0
-  for (int i = 1; i < PIXEL_COUNT; i++) lamp[i].set_hsv(g_color_palette[g_color_palette_index].h, g_color_palette[g_color_palette_index].s, 0.0);
+  for (int i = 1; i < PIXEL_COUNT; i++) lamp[i].set_hsv(patconf_color_palette[patvar_color_palette_index].h, patconf_color_palette[patvar_color_palette_index].s, 0.0);
 }
 
 void process_colorWipe() {
   long current_step_time = millis();
-  int wait_interval= g_pattern_step_wait_index==STEP_ON_2BEATS? g_output_waittime[g_pattern_step_wait_index]:g_output_waittime[g_pattern_step_wait_index]/3;
+  int wait_interval= patconf_speed_id==STEP_ON_2BEATS? output_waittime[patconf_speed_id]:output_waittime[patconf_speed_id]/3;
 
-  if (current_step_time - g_pattern_previous_step_time < wait_interval) return;
-  g_pattern_previous_step_time = current_step_time;
+  if (current_step_time - output_pattern_previous_step_time < wait_interval) return;
+  output_pattern_previous_step_time = current_step_time;
 
   // Determine the lamp, this step will change
-  int lamp_index = LAMP_COUNT - (g_pattern_step_index % (LAMP_COUNT - 1)) - 1;
+  int lamp_index = LAMP_COUNT - (patvar_current_step_index % (LAMP_COUNT - 1)) - 1;
 
   // Depending on phase, switch on or off
-  if (g_pattern_step_index < LAMP_COUNT - 1)
-    lamp[lamp_index].set_hsv(g_color_palette[g_color_palette_index].h, g_color_palette[g_color_palette_index].s, g_pattern_value);
+  if (patvar_current_step_index < LAMP_COUNT - 1)
+    lamp[lamp_index].set_hsv(patconf_color_palette[patvar_color_palette_index].h, patconf_color_palette[patvar_color_palette_index].s, patconf_pattern_lamp_value);
   else
     lamp[lamp_index].set_value(0.0);
 
@@ -273,10 +272,10 @@ void process_colorWipe() {
   output_push_lamps_to_pixels();
   
   // Step foreward
-  if (++g_pattern_step_index >= g_max_step_count) {
-    g_pattern_step_index = 0;
+  if (++patvar_current_step_index >= patconf_max_step_count) {
+    patvar_current_step_index = 0;
     // Change color after complete cycle
-    if (++g_color_palette_index>=g_color_palette_lenght) g_color_palette_index=0;
+    if (++patvar_color_palette_index>=patconf_color_palette_lenght) patvar_color_palette_index=0;
   }
 }
 
@@ -286,45 +285,94 @@ void process_colorWipe() {
 void start_doubleOrbit(float lamp_value,  int steps_per_color){
   // init all globales for the pattern
   g_current_stepper_type=ST_DOUBLE_ORBIT;
-  g_pattern_previous_step_time = output_get_current_beat_start_time();
-  g_pattern_step_index = 0;
-  g_steps_per_color=steps_per_color;
-  g_step_in_color_index=0;
+  output_pattern_previous_step_time = output_get_current_beat_start_time();
+  patvar_current_step_index = 0;
+  patconf_steps_until_color_change=steps_per_color;
+  patvar_step_in_color_index=0;
 
-  g_pattern_value = lamp_value;
-  g_color_palette_index=0;
+  patconf_pattern_lamp_value = lamp_value;
+  patvar_color_palette_index=0;
 
   // init all lamps
   lamp[0].set_value(0.0); // Switch of center lamp
   // Preset color, but set value to 0
-  for (int i = 1; i < PIXEL_COUNT; i++) lamp[i].set_hsv(g_color_palette[g_color_palette_index].h, g_color_palette[g_color_palette_index].s, 0.0);
+  for (int i = 1; i < PIXEL_COUNT; i++) lamp[i].set_hsv(patconf_color_palette[patvar_color_palette_index].h, patconf_color_palette[patvar_color_palette_index].s, 0.0);
 }
 
 void process_doubleOrbit() {
 
   long current_step_time = millis();
-  if (current_step_time - g_pattern_previous_step_time < g_output_waittime[g_pattern_step_wait_index]) return;
-  g_pattern_previous_step_time = current_step_time;
+  if (current_step_time - output_pattern_previous_step_time < output_waittime[patconf_speed_id]) return;
+  output_pattern_previous_step_time = current_step_time;
 
-  int angle = g_pattern_step_index % 3; // angle, where the pixel is on
+  int angle = patvar_current_step_index % 3; // angle, where the pixel is on
 
   for (int lamp_index = 1; lamp_index < LAMP_COUNT; ++lamp_index) {
-    if (lamp_index % 3 == angle) lamp[lamp_index].set_hsv(g_color_palette[g_color_palette_index].h, g_color_palette[g_color_palette_index].s, g_pattern_value);
+    if (lamp_index % 3 == angle) lamp[lamp_index].set_hsv(patconf_color_palette[patvar_color_palette_index].h, patconf_color_palette[patvar_color_palette_index].s, patconf_pattern_lamp_value);
     else  lamp[lamp_index].set_value(0.0);
   }
 
   // center pixel lightens every 4th step for 2 ticks
-  if (g_pattern_step_index % 8 == 0 || g_pattern_step_index % 8 == 1) lamp[0].set_hsv(g_color_palette[g_color_palette_index].h, g_color_palette[g_color_palette_index].s, g_pattern_value);
+  if (patvar_current_step_index % 8 == 0 || patvar_current_step_index % 8 == 1) lamp[0].set_hsv(patconf_color_palette[patvar_color_palette_index].h, patconf_color_palette[patvar_color_palette_index].s, patconf_pattern_lamp_value);
   else  lamp[0].set_value(0.0);
 
   output_push_lamps_to_pixels();
 
   // step foreward;
   
-  if (++g_pattern_step_index >= 24) g_pattern_step_index = 0; // Whole sequence has 24 step because of the blinking
-  if (++g_step_in_color_index>=g_steps_per_color) {
-        g_step_in_color_index=0;
-        if (++g_color_palette_index>=g_color_palette_lenght) g_color_palette_index=0;
+  if (++patvar_current_step_index >= 24) patvar_current_step_index = 0; // Whole sequence has 24 step because of the blinking
+  if (++patvar_step_in_color_index>=patconf_steps_until_color_change) {
+        patvar_step_in_color_index=0;
+        if (++patvar_color_palette_index>=patconf_color_palette_lenght) patvar_color_palette_index=0;
+  }
+}
+
+/*
+ *   Double Orbit
+ */
+void start_doubleColorOrbit(float lamp_value,  int steps_per_color, int color_increment){
+  // init all globales for the pattern
+  g_current_stepper_type=ST_DOUBLE_COLOR_ORBIT;
+  output_pattern_previous_step_time = output_get_current_beat_start_time();
+  patvar_current_step_index = 0;
+  patconf_steps_until_color_change=steps_per_color;
+  patvar_step_in_color_index=0;
+  patconf_step_color_increment=color_increment;
+
+  patconf_pattern_lamp_value = lamp_value;
+  patvar_color_palette_index=0;
+
+  // init all lamps
+  lamp[0].set_value(0.0); // Switch of center lamp
+  // Preset color, but set value to 0
+  for (int i = 1; i < PIXEL_COUNT; i++) lamp[i].set_hsv(patconf_color_palette[patvar_color_palette_index].h, patconf_color_palette[patvar_color_palette_index].s, 0.0);
+}
+
+void process_doubleColorOrbit() {
+
+  long current_step_time = millis();
+  if (current_step_time - output_pattern_previous_step_time < output_waittime[patconf_speed_id]) return;
+  output_pattern_previous_step_time = current_step_time;
+
+  int angle = patvar_current_step_index % 3; // angle, where the pixel is on
+
+  for (int lamp_index = 1; lamp_index < LAMP_COUNT; ++lamp_index) {
+    if (lamp_index % 3 == angle) lamp[lamp_index].set_hsv(patconf_color_palette[patvar_color_palette_index].h, patconf_color_palette[patvar_color_palette_index].s, patconf_pattern_lamp_value);
+    else  lamp[lamp_index].set_value(0.0);
+  }
+
+  // center pixel lightens every 4th step for 2 ticks
+  if (patvar_current_step_index % 8 == 0 || patvar_current_step_index % 8 == 1) lamp[0].set_hsv(patconf_color_palette[patvar_color_palette_index].h, patconf_color_palette[patvar_color_palette_index].s, patconf_pattern_lamp_value);
+  else  lamp[0].set_value(0.0);
+
+  output_push_lamps_to_pixels();
+
+  // step foreward;
+  
+  if (++patvar_current_step_index >= 24) patvar_current_step_index = 0; // Whole sequence has 24 step because of the blinking
+  if (++patvar_step_in_color_index>=patconf_steps_until_color_change) {
+        patvar_step_in_color_index=0;
+        if (++patvar_color_palette_index>=patconf_color_palette_lenght) patvar_color_palette_index=0;
   }
 }
 
@@ -336,11 +384,11 @@ void process_doubleOrbit() {
   // init all globales for the pattern
 
   g_current_stepper_type=ST_RAINBOW;
-     g_pattern_previous_step_time = output_get_current_beat_start_time();
+     output_pattern_previous_step_time = output_get_current_beat_start_time();
  
-  g_rainbow_step_angle_increment=angle_step;
+  patconf_rainbow_step_angle_increment=angle_step;
    
-  g_pattern_value = lamp_value;
+  patconf_pattern_lamp_value = lamp_value;
   
   // init all lamps
   lamp[0].set_value(0.0); // Switch of center lamp
@@ -356,11 +404,11 @@ void process_doubleOrbit() {
 void process_rainbow() {
 
   long current_step_time = millis();
-  if (current_step_time - g_pattern_previous_step_time <  g_output_waittime[g_pattern_step_wait_index]) return;
-  g_pattern_previous_step_time = current_step_time;
+  if (current_step_time - output_pattern_previous_step_time <  output_waittime[patconf_speed_id]) return;
+  output_pattern_previous_step_time = current_step_time;
 
   for (int lamp_index = 1; lamp_index < LAMP_COUNT; ++lamp_index) {
-    lamp[lamp_index].add_hue_angle(g_rainbow_step_angle_increment);
+    lamp[lamp_index].add_hue_angle(patconf_rainbow_step_angle_increment);
   }
 
   output_push_lamps_to_pixels();
@@ -375,7 +423,7 @@ void output_push_lamps_to_pixels()
 {
   t_color_rgb_int lamp_color;
   for (int p = 0; p < PIXEL_COUNT; p++) {
-    lamp_color = lamp[p].get_color_rgb( g_master_light_value);
+    lamp_color = lamp[p].get_color_rgb( output_master_light_value);
     strip.setPixelColor(p, strip.Color(lamp_color.r, lamp_color.g, lamp_color.b ));
     
 #ifdef TRACE_OUPUT_PIXEL_RESULT
